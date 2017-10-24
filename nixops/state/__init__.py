0d84b76092c1859802bfbcfee75f73ec15a85a4b
Tal Z <tal.z@atidot.com>
2017-10-24 16:01:28 +0300

    added support for sql and vault in the 'state' flag. for vault, supply the vault url in the form of vault.*.*, for sqlite, dont supply anything, or the default directory

--- nixops/state/__init__.py
+++ nixops/state/__init__.py
@@ -3,26 +3,24 @@ import sys
 import sqlite3_file
 import json_file
 import vault_file
+import pdb
 
 class WrongStateSchemeException(Exception):
     pass
 
+#vault_backend = "vault"
+#sqlite_backend = "sqlite"
+#json_backend = "json"
+
+
 def open(url):
     url = urlparse.urlparse(url)
-    scheme = url.scheme
 
-    #if scheme == "":
-    scheme = "vault"
+    if url.netloc == '' and 'deployments.nixops' in url.path.split('/'): # no internet loc and checking for the deployment db name 
+        return sqlite3_file.StateFile(url.path) 
+    elif url.netloc == '' and url.path[-4:] == "json":
+        return json_file.JsonFile(url.path)
+    elif "vault" in url.netloc.split('.') or "vault" in url.path.split('.'):
+        return vault_file.VaultState()
 
-    def raise_(ex):
-        raise ex
-
-    switcher = {
-        "sqlite3": lambda(url): sqlite3_file.StateFile(url.path),
-        "json": lambda(url): json_file.JsonFile(url.path),
-        "vault": lambda(url): vault_file.VaultState(),
-
-    }
-
-    function = switcher.get(scheme, lambda(url): raise_(WrongStateSchemeException("Unknown state scheme!")))
-    return function(url)
+    raise WrongStateSchemeException("Unknown state scheme! vault can be connect only by suppling it's domain name")
